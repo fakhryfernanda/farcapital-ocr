0af0663c51c989165911437f73c798049375a98c
fakhryfernanda <mff023@gmail.com>
2023-01-05 14:32:26 +0700

    (+) script cdn fontawesome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://kit.fontawesome.com/e0f8177fe6.js" crossorigin="anonymous"></script>
     @vite('resources/css/app.css')
     <title>Document</title>
     @livewireStyles
