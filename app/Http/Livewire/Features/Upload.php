76c76561531a970a447076a44c4165f5a52fe571
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-05 10:25:15 +0700

    (+) livewire upload

---
+++ app/Http/Livewire/Features/Upload.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Features;
+
+use Livewire\Component;
+
+class Upload extends Component
+{
+    public function render()
+    {
+        return view('livewire.features.upload');
+    }
+}
