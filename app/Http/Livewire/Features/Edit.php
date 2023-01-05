0e89874b811da202157ba4b7dd2118ef73f54ec6
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-05 13:51:40 +0700

    (+) tambah dummy

--- app/Http/Livewire/Features/Edit.php
+++ app/Http/Livewire/Features/Edit.php
@@ -6,6 +6,28 @@ use Livewire\Component;
 
 class Edit extends Component
 {
+    public $nama = 'fakhry';
+    public $nik ='330508030420000001';
+    public $tempat_lahir ='Bekasi';
+    public $tanggal_lahir = '04/03/2000';
+    public $jenis_kelamin = 'L'; 
+    public $golongan_darah = 'A';
+    public $alamat= 'Tambun selatan';
+    public $rt = '004';
+    public $rw = '004';
+    public $desa = 'Tambun selatan';
+    public $kecamatan = 'Tambun';
+    public $agama = 'Islam';
+    public $status_perkawinan = 'Belum nikah';
+    public $pekerjaan = 'programmer';
+    public $kewarganegaraan = 'WNI';
+    public $berlaku_hingga ='seumur jagung';
+
+    public function edit (){
+    
+    }
+
+    
     public function render()
     {
         return view('livewire.features.edit');
--- resources/views/livewire/features/edit.blade.php
+++ resources/views/livewire/features/edit.blade.php
@@ -1,90 +1,110 @@
 <div>
     {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
     <div class="container mt-10 block p-6 rounded-lg shadow-lg bg-blue-100 max-w-md border-2 w-full">
-        <form action="">
+        <form method="POST" wire:submit.prevent="update" enctype="multipart/form-data">
+            @csrf
             <h1 class=" flex justify-center text-[30px] text-blue-400 mb-5">Biodata Diri</h1>
             <div class="flex items-center">
                 <label class="w-40" for="nama">Nama : </label>
                 <input class="border-2  focus:border-blue-600 focus:outline-none" type="text" name="nama" id="nama"
-                    wire:click="nama" value="fahri"><br><br>
+                    wire:model="nama" ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="nik">NIK : </label>
-                <input class="border-2" type="text" name="nik" id="nik" wire:click="nik"
-                    value="3305081212120001"><br><br>
+                <input class="border-2" type="text" name="nik" id="nik" wire:model="nik"
+                    ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="tempat_lahir">Tempat Lahir : </label>
-                <input class="border-2" type="text" name="tempat_lahir" id="tempat_lahir" value="Bekasi"
-                    wire:click="tempat_lahir"><br><br>
+                <input class="border-2" type="text" name="tempat_lahir" id="tempat_lahir" 
+                    wire:model="tempat_lahir"><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="tanggal_lahir">Tanggal Lahir : </label>
                 <input class="border-2" type="date" name="tanggal_lahir" id="tanggal_lahir"
-                    wire:click="tanggal_lahir"><br><br>
+                    wire:model="tanggal_lahir"><br><br>
             </div>
             <!-- <div class="flex gap-40"> -->
             <div class="flex items-center">
                 <label class="w-40" for="jenis_kelamin">Jenis Kelamin : </label>
-                <input class="border-2 gap-20" type="text" name="jenis_kelamin" id="jenis_kelamin"
-                    wire:click="golongan_darah" value="L"><br><br>
+                <select  class="border-2 gap-20" name="jenis_kelamin" id="jenis_kelamin" wire:model="jenis_kelamin">
+                    <option value="L">L</option>
+                    <option value="P">P</option>
+                </select><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="golongan_darah">Golongan Darah : </label>
-                <input class="border-2" type="text" name="golongan_darah" id="golongan_darah"
-                    wire:click="golongan_darah" value="A"><br><br>
+                <select  class="border-2 gap-20" name="golongan_darah" id="golongan_darah" wire:model="golongan_darah">
+                    <option value="A">A</option>
+                    <option value="B">B</option>
+                    <option value="AB">AB</option>
+                    <option value="O">O</option>
+                </select><br><br>
             </div>
             <!-- </div> -->
             <div class="flex items-center">
                 <label class="w-40" for="alamat">Alamat : </label>
-                <input class="border-2" type="text" name="nama" id="nama" wire:click="nama"
-                    value="Kampung Rambutan"><br><br>
+                <input class="border-2" type="text" name="nama" id="nama" wire:model="alamat"
+                   ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="rt">RT : </label>
-                <input class="border-2" type="text" name="rt" id="rt" wire:click="rt" value="004"><br><br>
+                <input class="border-2" type="text" name="rt" id="rt" wire:model="rt" ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="rw">RW : </label>
-                <input class="border-2" type="text" name="rw" id="rw" wire:click="rw" value="004"><br><br>
+                <input class="border-2" type="text" name="rw" id="rw" wire:model="rw" ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="desa">Desa : </label>
-                <input class="border-2" type="text" name="desa" id="desa" wire:click="desa"
-                    value="Tambun Selatan"><br><br>
+                <input class="border-2" type="text" name="desa" id="desa" wire:model="desa"
+                    ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="kecamatan">Kecamatan : </label>
-                <input class="border-2" type="text" name="kecamatan" id="kecamatan" wire:click="kecamatan"
-                    value="Tambun"><br><br>
+                <input class="border-2" type="text" name="kecamatan" id="kecamatan" wire:model="kecamatan"
+                    ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="agama">Agama : </label>
-                <input class="border-2" type="text" name="agama" id="agama" wire:click="agama" value="Islam"><br><br>
+                <select  class="border-2 gap-20" name="agama" id="agama" wire:model="agama">
+                    <option value="Islam">Islam</option>
+                    <option value="Kristen">Kristen</option>
+                    <option value="Katolik">Katolik</option>
+                    <option value="Konghucu">Konghucu</option>
+                    <option value="Budha">Budha</option>
+                    <option value="Hindu">Hindu</option>
+                </select><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="status_perkawinan">Status Perkawinan : </label>
-                <input class="border-2" type="text" name="status_perkawinan" id="status_perkawinan"
-                    wire:click="status_perkawinan" value="Belum Kawin"><br><br>
+                <select  class="border-2 gap-20" name="status_perkawinan" id="status_perkawinan" wire:model="status_perkawinan">
+                    <option value="Belum_kawin">Belum Kawin</option>
+                    <option value="Kawin">Kawin</option>
+                    <option value="Janda">Janda</option>
+                    <option value="Duda">Duda</option>
+                </select><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="pekerjaan">Pekerjaan : </label>
-                <input class="border-2" type="text" name="pekerjaan" id="pekerjaan" wire:click="pekerjaan"
-                    value="Programmer"><br><br>
+                <input class="border-2" type="text" name="pekerjaan" id="pekerjaan" wire:model="pekerjaan"
+                    ><br><br>
             </div>
             <div class="flex items-center">
                 <label class="w-40" for="kewarganegaraan">Kewarganegaraan : </label>
-                <input class="border-2" type="text" name="kewarganegaraan" id="kewarganegaraan"
-                    wire:click="kewarganegaraan" value="WNI"><br><br>
+                <select  class="border-2 gap-20" name="kewarganegaraan" id="kewarganegaraan" wire:model="kewarganegaraan">
+                    <option value="WNI">WNI</option>
+                    <option value="WNA">WNA</option>
+                </select><br><br>
             </div>
 
             <div class="flex items-center">
                 <label class="w-40" for="berlaku_hingga">Berlaku Hingga : </label>
                 <input class="border-2" type="text" name="berlaku_hingga" id="berlaku_hingga"
-                    wire:click="berlaku_hingga" value="Seumur jagung"><br><br>
+                    wire:model="berlaku_hingga" ><br><br>
             </div>
             <div class="w-full flex justify-center">
-                <button
+                <button wire:model=""
                     class="inline-block   mt-7 px-7 py-2 bg-green-400 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-yellow-600 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-yellow-600 active:shadow-lg transition duration-150 ease-in-out">
                     Edit</button>
             </div>
