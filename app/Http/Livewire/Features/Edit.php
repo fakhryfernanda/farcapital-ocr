0638f9096c5d855e8d27586cce9c559336e72fb9
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-05 10:29:24 +0700

    (+) livewire edit (+) view form edit

---
+++ app/Http/Livewire/Features/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Features;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.features.edit');
+    }
+}
