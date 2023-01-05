8039bbf84c8e8ba410a30cfd84b827f715cc8234
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-05 10:27:35 +0700

    (+) livewire profile

---
+++ app/Http/Livewire/Features/Profile.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Features;
+
+use Livewire\Component;
+
+class Profile extends Component
+{
+    public function render()
+    {
+        return view('livewire.features.profile');
+    }
+}
