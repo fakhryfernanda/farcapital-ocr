ef87925e5fa205666034734e9a844299d29253e5
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-05 10:26:48 +0700

    (+) livewire registrasi

---
+++ app/Http/Livewire/Auth/Registrasi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+
+class Registrasi extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.registrasi');
+    }
+}
