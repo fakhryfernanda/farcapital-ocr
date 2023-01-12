94909d4f7d6e85e2b733ae246aeb3987e2bcb8ad
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-12 13:44:42 +0700

    (+) halaman succes send email

---
+++ app/Http/Livewire/Auth/SuccessSendEmail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+
+class SuccessSendEmail extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.success-send-email');
+    }
+}
