0ca6e5b0e5789c6c8818b44cb4584a8ecd15cde2
fakhryfernanda <mff023@gmail.com>
2023-01-12 10:40:57 +0700

    (+) cek apakah sudah memiliki data identitas

--- app/Http/Livewire/Features/Upload.php
+++ app/Http/Livewire/Features/Upload.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire\Features;
 
 use Livewire\Component;
+use App\Helpers\HttpClient;
 
 class Upload extends Component
 {
+    public function mount() 
+    {
+        $id = session('id_user');
+
+        $responseData = HttpClient::fetch(
+            "GET",
+            "http://localhost:8000/api/identity/{$id}"
+        );
+
+        if ($responseData['status']) {
+            return redirect('/profile');
+        }
+    }
+
     public function render()
     {
         return view('livewire.features.upload');
