e00f84ddf0f9d2364b8c7274f25c36a5cb9d074d
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-12 13:43:46 +0700

    (+) halaman change password

---
+++ app/Http/Livewire/Auth/ChangePassword.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+
+class ChangePassword extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.change-password');
+    }
+}
