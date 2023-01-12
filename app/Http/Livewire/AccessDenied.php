dff056d30ec92e18501d2c2ec6984d704324c47f
fakhryfernanda <mff023@gmail.com>
2023-01-12 11:12:14 +0700

    (+) livewire accessdenied

---
+++ app/Http/Livewire/AccessDenied.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class AccessDenied extends Component
+{
+    public function render()
+    {
+        return view('livewire.access-denied');
+    }
+}
