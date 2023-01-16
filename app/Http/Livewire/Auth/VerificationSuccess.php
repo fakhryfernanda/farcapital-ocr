0c59f9401562cc658c3ed8988aca1ad564798865
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-16 15:27:27 +0700

    (+) komponen verifikasi sukses

---
+++ app/Http/Livewire/Auth/VerificationSuccess.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Auth;
+
+use Livewire\Component;
+
+class VerificationSuccess extends Component
+{
+    public function render()
+    {
+        return view('livewire.auth.verification-success');
+    }
+}
