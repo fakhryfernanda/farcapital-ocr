f29492f8c96bc7b4ce762b7d8f55399ffce3a11e
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-20 13:25:58 +0700

    (-) jenis kelamin

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -24,19 +24,22 @@
                         Jenis Kelamin
                     </th>
                     
-                    <th scope="col" class="text-sm font-medium  px-6 py-4 text-left">
+                    {{-- <th scope="col" class="text-sm font-medium  px-6 py-4 text-left">
                         Foto
-                    </th>
+                    </th> --}}
                      <th scope="col" class="text-sm font-medium  px-6 py-4 text-left">
                         Aksi
                     </th>
                 </tr>
             </thead>
             <tbody>
-                <template x-for="user in users" :key= "user.id">
+                <template x-for="(user, index) in users" :key= "user.id">
                     <tr class="bg-white border-2">
-                        <td  class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900"><h1>1</h1>
-                            {{-- {{ $loop->iteration }} --}}
+                        <td x-text="index+1" class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                            {{-- <template x-for="index in user">
+                                <h1 x-text="index + 1"></h1>
+                            </template> --}}
+                            {{-- <h1 x-text="user.id"></h1> --}}
                         </td>
                          <td x-text="user.nik" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                            
@@ -50,12 +53,21 @@
                            
                         </td>
                         
-                         <td x-text="user.ktp" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                         {{-- <td x-text="user.ktp" class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"> --}}
                             {{-- <img src="{{  'http://127.0.0.1:8000/storage/' .  $user["ktp"] }}" alt="ktp" class="w-[100px]"> --}}
-                        </td>
+                        {{-- </td> --}}
                          <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                             {{-- <a href={{ route('profile', $user["id_user"]) }}> --}}
-                                <button class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Detail</button>
+
+                                <button x-on:click="window.location.href = '/profile + id'" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">Detail</button>
+                                <script>
+                                    function redirect(){
+                                        // window.location.href="profile/{id}";
+                                        // window.location.replace(this.baseUrl + '/profile/{id}')
+                                        const baseUrl = window.location.origin
+                                    window.location.replace(baseUrl + '/profile/{id}')
+                                    }
+                                </script>
                             {{-- </a> --}}
                         </td>
                     </tr>
