946a14386538d9025581dcf274c69d994118d26c
fakhryfernanda <mff023@gmail.com>
2023-01-23 17:18:48 +0700

    (+) memisahkan kode login (+) validasi email dan password

--- public/js/auth.js
+++ public/js/auth.js
@@ -63,3 +63,110 @@ Alpine.data('userRegister', () => ({
 
     }
 }))
+
+Alpine.data('userLogin', () => ({
+    email: '',
+    password: '',
+
+    errmsg: '',
+    errarea: '',
+    message: '',
+
+    isloading: false,
+
+    flash: false,
+    flashdata() {
+        if (localStorage.getItem('flash')) {
+            this.flash = true
+            this.message = localStorage.getItem('message')
+            setTimeout(function () {
+                localStorage.removeItem('flash')
+                localStorage.removeItem('message')
+                this.flash = false
+            }, 3000)
+        }
+    },
+
+    submit() {
+        if (this.email.length == 0) {
+            this.errarea = 'email'
+            this.errmsg = 'Email wajib diisi'
+        } else if (this.password.length == 0) {
+            this.errarea = 'password'
+            this.errmsg = 'Password wajib diisi'
+        } else {
+            this.isloading = true
+            const data = new FormData();
+            data.append('email', this.email)
+            data.append('password', this.password)
+
+            fetch(beapi + 'login', {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                },
+                body: data
+            })
+                .then(async response => {
+                    response = await response.json()
+                    this.message = response.message
+                    responsestatus = response.status
+
+                    // Jika login berhasil
+                    if (responsestatus) {
+                        auth = response.data.auth
+                        user = response.data.user
+                        utoken = auth.token_type + ' ' + auth.token
+                        urole = user.id_role
+                        uid = user.id
+
+                        localStorage.setItem('utoken', utoken)
+                        localStorage.setItem('urole', urole)
+                        localStorage.setItem('uid', uid)
+
+                        // Jika yang login admin
+                        if (urole == 1) {
+                            const baseUrl = window.location.origin
+                            window.location.replace(baseUrl + '/dashboard')
+                        }
+
+                        // Jika yang login user
+                        else if (urole == 2) {
+                            // Memeriksa apakah user sudah pernah scan ktp
+                            fetch(beapi + 'identity/' + uid, {
+                                method: 'GET',
+                                headers: {
+                                    'Accept': 'application/json',
+                                    'Authorization': utoken
+                                },
+                            })
+                                .then(async response => {
+                                    response = await response.json()
+                                    this.message = response.message
+                                    statuslogin = response.status
+
+                                    // User sudah scan
+                                    if (statuslogin) {
+                                        const baseUrl = window.location.origin
+                                        window.location.replace(baseUrl + '/profile')
+                                    }
+                                    
+                                    // User belum scan
+                                    else {
+                                        const baseUrl = window.location.origin
+                                        window.location.replace(baseUrl + '/scan')
+                                    }
+                                })
+                        }
+                    }
+
+                    // Jika login gagal
+                    else {
+                        this.errarea = response.data
+                        this.errmsg = this.message
+                        this.isloading = false
+                    }
+                })
+        }
+    }
+}))
