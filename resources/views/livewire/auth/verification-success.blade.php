77375ce68c54d55b66231f604bf6f4b759fa7349
fakhryfernanda <mff023@gmail.com>
2023-01-23 17:25:18 +0700

    (~) mengganti ukuran div dan font

--- resources/views/livewire/auth/verification-success.blade.php
+++ resources/views/livewire/auth/verification-success.blade.php
@@ -1,16 +1,16 @@
-<div x-data="successvalidation" x-init="cektoken()">
-    <div class=" bg-white w-full px-10 pb-10 flex flex-col gap-10 ">
+<div x-data="successvalidation" x-init="cektoken()" class="min-h-1/2">
+    <div class=" bg-white lg:w-[500px] w-full rounded-xl shadow-lg shadow-gray-600/50  flex flex-col items-center text-center md:px-16 px-0 pt-7 pb-8 z-0">
         <div class="flex flex-col">
             <div class=" flex justify-center">
                 <img src="{{ asset('assets/download.png')}}"> 
             </div>
             <h1 class="font-bold flex justify-center ">Email verification status</h1>
             
-                <p class="text-[10px] text-center">Your email was verified
+                <p class="text-center py-3">Your email was verified
                 </p>
         </div>
         <input type="hidden" id="token" value="{{$token}}">
-        <a href="{{route('login')}}" type="button" class=" text-center border-blue-500 border  block  p-2 text-sm rounded-lg  bg-blue-600 font-semibold hover:bg-white hover:text-blue-500 hover:font-bold focus:outline-none hover:border-blue-500 hover:border w-full">Kembali ke login</a>
+        <a href="{{route('login')}}" type="button" class=" text-center border-red-800 border  block  p-2 text- rounded-lg  bg-red-900 font-semibold hover:bg-red-800 text-white focus:outline-none hover:border-red-900 hover:border w-[150px]">Back to Login</a>
     </div>
 
 </div>
