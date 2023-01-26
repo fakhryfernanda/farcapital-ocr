e7f5c09317be73b14ef880fc99b4d25dc2ab70e9
Ridwan RI1 <ridwanacer58@gmail.com>
2023-01-26 11:30:28 +0700

    (~) hompage

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,3 +1,15 @@
 <div class="bg-white shadow-md p-4 container text-center">
-    <h1 class="text-4xl font-bold">Homepage</h1>
+    
+    <div class="  space-x-2 flex justify-center">
+        <img src="{{asset('assets/download.png')}}" alt="">
+    </div>
+    <h1 class="text-xl font-bold pb-2 font-Lato">Welcome To</h1>
+    <p class="text-4xl font-Lato pb-10"> <span class="text-gray-700">Far</span> <span class="text-red-900"> Capital</span> OCR</p>
+    {{-- <div class="flex items-center justify-center">
+        <img src="{{asset('assets/logo.png')}}" alt=""> 
+    </div> --}}
+
 </div>
+{{-- <section class="container mb-8" x-data="profile">
+ 
+</section> --}}
