c34490170099a9bf1e41b246d7da46db836f056e
endjiansyah <endji1507@gmail.com>
2023-01-26 13:51:02 +0700

    hapus komentar di home.blade.php

--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -5,11 +5,5 @@
     </div>
     <h1 class="text-xl font-bold pb-2 font-Lato">Welcome To</h1>
     <p class="text-4xl font-Lato pb-10"> <span class="text-gray-700">Far</span> <span class="text-red-900"> Capital</span> OCR</p>
-    {{-- <div class="flex items-center justify-center">
-        <img src="{{asset('assets/logo.png')}}" alt=""> 
-    </div> --}}
 
 </div>
-{{-- <section class="container mb-8" x-data="profile">
- 
-</section> --}}
